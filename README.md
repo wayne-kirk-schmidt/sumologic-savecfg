6738c70ee43dca52be173fef788e74dc4d112244
Wayne Schmidt <wschmidt@sumologic.com>
2022-05-03 01:09:38 +0900

    initial release

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
-# sumologic-savecfg
+Sumo Logic Save Configuration
+=============================
+
+For you Sumo Logic environment as code, you know you want to backup and restore content but what about confiiguration changes?
+
+We have just the thing; Sumo Logic integration to allow you to report and archive your key configuration items.
+
+And, in the spirit of "Sumo on Sumo" we can also publish the record of configuration changes into Sumo Logic to query.
+
+Installing the Scripts
+=======================
+
+The scripts are CLI based, designed as a stand alone CLI or incorporated in a DevOPs tool such as Chef or Ansible.
+
+Written in python3, all scripts are listed below, and there is a Pipfile to show what modules are required to run.
+
+You will need to use Python 3.6 or higher and the modules listed in the dependency section.  
+
+Please follow the following steps to install:
+
+    1. Download and install python 3.6 or higher from python.org. Append python3 to the LIB and PATH env.
+
+    2. Download and install git for your platform if you don't already have it installed.
+       It can be downloaded from https://git-scm.com/downloads
+    
+    3. Open a new shell/command prompt. It must be new since only a new shell will include the new python 
+       path that was created in step 1. Cd to the folder where you want to install the scripts.
+    
+    4. Execute the following command to install pipenv, which will manage all of the library dependencies:
+    
+        sudo -H pip3 install pipenv 
+ 
+    5. Clone this repository and change into the directory.
+
+    6. run pipenv to install dependencies (this may take a while as this will download required libraries):
+
+        pipenv install
+        
+Dependencies
+============
+
+See the contents of "pipfile"
+
+Script Names and Purposes
+=========================
+
+Scripts and Functions:
+
+    1. ./bin/sumologic_savecfg.py -h
+  
+       print a usage message and exit
+
+    2. ./bin/sumologic_savecfg.py -c <cfgile>
+
+       use a configuration file for the API key name, string, API endpoint, and organization ID
+
+    3. ./bin/sumologic_savecfg.py -v <integer>
+
+       provide increasingly verbose output on the script execution
+
+    4. ./bin/sumologic_savecfg.py -p <publishtarget>
+
+       publish the output of the script to a Sumo Logic source Category
+
+    5. ./bin/sumologic_savecfg.py -a <apikey>:<apisecret>
+
+       specify the API key to use to connect to the Sumo Logic environment
+
+Uncoming Features:
+==================
+
+* Provide different output formats in addition to CSV and stdout
+
+License
+=======
+
+Copyright 2022 Wayne Kirk Schmidt
+https://www.linkedin.com/in/waynekirkschmidt
+
+Licensed under the Apache 2.0 License (the "License");
+
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    license-name   APACHE 2.0
+    license-url    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Support
+=======
+
+Feel free to e-mail me with issues to: 
+
+*    wschmidt@sumologic.com
+
+*    wayne.kirk.schmidt@gmail.com
+
+I will provide "best effort" fixes and extend the scripts.
---
+++ bin/sumologic_savecfg.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Exaplanation: sumologic_list_objects is a general object retrieval script
+
+Usage:
+   $ python  sumologic_list_objects [ options ]
+
+Style:
+   Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+    @name           sumocli_list_objects
+    @version        1.00
+    @author-name    Wayne Schmidt
+    @author-email   wschmidt@sumologic.com
+    @license-name   Apache 2.0
+    @license-url    http://www.gnu.org/licenses/gpl.html
+"""
+
+__version__ = 1.00
+__author__ = "Wayne Schmidt (wschmidt@sumologic.com)"
+
+### beginning ###
+import os
+import sys
+import time
+import datetime
+import argparse
+import configparser
+import importlib
+import json
+import http
+import requests
+sys.dont_write_bytecode = 1
+
+RIGHTNOW = datetime.datetime.now()
+
+DATESTAMP = RIGHTNOW.strftime('%Y%m%d')
+
+TIMESTAMP = RIGHTNOW.strftime('%H%M%S')
+
+BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+BINDIR = os.path.abspath(os.path.join(BASEDIR, 'bin'))
+ETCDIR = os.path.abspath(os.path.join(BASEDIR, 'etc'))
+LIBDIR = os.path.abspath(os.path.join(BASEDIR, 'lib'))
+
+sys.path.append(LIBDIR)
+
+JSONCFGTAG = 'sumologic_savecfg.json'
+
+MY_CFG = 'undefined'
+
+PARSER = argparse.ArgumentParser(description="""
+sumologic_list_objects is a general object retrieval script
+""")
+
+PARSER.add_argument("-a", metavar='<secret>', dest='MY_SECRET', \
+                    help="set api (format: <key>:<secret>) ")
+
+PARSER.add_argument("-k", metavar='<client>', dest='MY_CLIENT', \
+                    help="set key (format: <site>_<orgid>) ")
+
+PARSER.add_argument("-c", metavar='<configfile>', dest='CONFIG', \
+                    help="Specify config file")
+PARSER.add_argument("-m", metavar='<modulelist>', dest='MODULENAME', \
+                    default='all', help="Specify config file")
+
+PARSER.add_argument("-v", type=int, default=0, metavar='<verbose>', \
+                    dest='verbose', help="increase verbosity")
+
+ARGS = PARSER.parse_args()
+
+def resolve_option_variables():
+    """
+    Validates and confirms all necessary variables for the script
+    """
+
+    if ARGS.MY_SECRET:
+        (keyname, keysecret) = ARGS.MY_SECRET.split(':')
+        os.environ['SUMO_UID'] = keyname
+        os.environ['SUMO_KEY'] = keysecret
+
+    if ARGS.MY_CLIENT:
+        (deployment, organizationid) = ARGS.MY_CLIENT.split('_')
+        os.environ['SUMO_LOC'] = deployment
+        os.environ['SUMO_ORG'] = organizationid
+
+def resolve_config_variables():
+    """
+    Validates and confirms all necessary variables for the script
+    """
+
+    if ARGS.CONFIG:
+        cfgfile = os.path.abspath(ARGS.CONFIG)
+        configobj = configparser.ConfigParser()
+        configobj.optionxform = str
+        configobj.read(cfgfile)
+
+        if ARGS.verbose > 8:
+            print('Displaying Config Contents:')
+            print(dict(configobj.items('Default')))
+
+        if configobj.has_option("Default", "SUMO_TAG"):
+            os.environ['SUMO_TAG'] = configobj.get("Default", "SUMO_TAG")
+
+        if configobj.has_option("Default", "SUMO_UID"):
+            os.environ['SUMO_UID'] = configobj.get("Default", "SUMO_UID")
+
+        if configobj.has_option("Default", "SUMO_KEY"):
+            os.environ['SUMO_KEY'] = configobj.get("Default", "SUMO_KEY")
+
+        if configobj.has_option("Default", "SUMO_LOC"):
+            os.environ['SUMO_LOC'] = configobj.get("Default", "SUMO_LOC")
+
+        if configobj.has_option("Default", "SUMO_END"):
+            os.environ['SUMO_END'] = configobj.get("Default", "SUMO_END")
+
+        if configobj.has_option("Default", "SUMO_ORG"):
+            os.environ['SUMO_ORG'] = configobj.get("Default", "SUMO_ORG")
+
+def initialize_variables():
+    """
+    Validates and confirms all necessary variables for the script
+    """
+
+    resolve_option_variables()
+
+    resolve_config_variables()
+
+    try:
+        my_uid = os.environ['SUMO_UID']
+        my_key = os.environ['SUMO_KEY']
+
+    except KeyError as myerror:
+        print(f'Environment Variable Not Set :: {myerror.args[0]}')
+
+    return my_uid, my_key
+
+( sumo_uid, sumo_key ) = initialize_variables()
+
+### script logic  ###
+def main():
+    """
+    Setup the Sumo API connection, using the required tuple of region, id, and key.
+    Once done, then issue the command required
+    """
+    source = SumoApiClient(sumo_uid, sumo_key)
+
+    jsoncfgfile = os.path.join(ETCDIR, JSONCFGTAG)
+
+    with open(jsoncfgfile, "r" ) as fileobject:
+
+        cfgobject = json.load(fileobject)
+
+        for cfgkey in cfgobject:
+
+            if ARGS.MODULENAME in cfgobject or ARGS.MODULENAME == 'all':
+
+                module = importlib.import_module(cfgkey, package=None)
+
+                output = module.get_and_format_output(source)
+
+                print(output)
+
+                time.sleep(3)
+
+
+
+### script logic  ###
+### class ###
+class SumoApiClient():
+    """
+    This is defined SumoLogic API Client
+    The class includes the HTTP methods, cmdlets, and init methods
+    """
+
+    def __init__(self, access_id, access_key, endpoint=None, cookieFile='cookies.txt'):
+        """
+        Initializes the Sumo Logic object
+        """
+        self.session = requests.Session()
+        self.session.auth = (access_id, access_key)
+        self.session.headers = {'content-type': 'application/json', \
+            'accept': 'application/json'}
+        cookiejar = http.cookiejar.FileCookieJar(cookieFile)
+        self.session.cookies = cookiejar
+        if endpoint is None:
+            self.apipoint = self._get_endpoint()
+        elif len(endpoint) < 3:
+            self.apipoint = 'https://api.' + endpoint + '.sumologic.com/api'
+        else:
+            self.apipoint = endpoint
+        if self.apipoint[-1:] == "/":
+            raise Exception("Endpoint should not end with a slash character")
+
+    def _get_endpoint(self):
+        """
+        SumoLogic REST API endpoint changes based on the geo location of the client.
+        It contacts the default REST endpoint and resolves the 401 to get the right endpoint.
+        """
+        self.endpoint = 'https://api.sumologic.com/api'
+        self.response = self.session.get('https://api.sumologic.com/api/v1/collectors')
+        endpoint = self.response.url.replace('/v1/collectors', '')
+        return endpoint
+
+    def delete(self, method, params=None, headers=None, data=None):
+        """
+        Defines a Sumo Logic Delete operation
+        """
+        response = self.session.delete(self.apipoint + method, \
+            params=params, headers=headers, data=data)
+        if response.status_code != 200:
+            response.reason = response.text
+        response.raise_for_status()
+        return response
+
+    def get(self, method, params=None, headers=None):
+        """
+        Defines a Sumo Logic Get operation
+        """
+        response = self.session.get(self.apipoint + method, \
+            params=params, headers=headers)
+        if response.status_code != 200:
+            response.reason = response.text
+        response.raise_for_status()
+        return response
+
+    def post(self, method, data, headers=None, params=None):
+        """
+        Defines a Sumo Logic Post operation
+        """
+        response = self.session.post(self.apipoint + method, \
+            data=json.dumps(data), headers=headers, params=params)
+        if response.status_code != 200:
+            response.reason = response.text
+        response.raise_for_status()
+        return response
+
+    def put(self, method, data, headers=None, params=None):
+        """
+        Defines a Sumo Logic Put operation
+        """
+        response = self.session.put(self.apipoint + method, \
+            data=json.dumps(data), headers=headers, params=params)
+        if response.status_code != 200:
+            response.reason = response.text
+        response.raise_for_status()
+        return response
+
+### class ###
+### methods ###
+
+    def get_object(self,objectpath):
+        """
+        Using an HTTP client, this uses a GET to retrieve information.
+        """
+        url = str(objectpath)
+        body = self.get(url).text
+        results = json.loads(body)
+        return results
+
+### methods ###
+if __name__ == '__main__':
+    main()
